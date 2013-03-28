e2ea8f08983c3625bbe3f67af4c0c36f5fedc88a
Ivan Tadic <tadic@mappi.helsinki.fi>
2013-03-28 22:20:42 +0200

    replSpecChars() method and test

--- Miniprojekti/src/main/java/controllers/Converter.java
+++ Miniprojekti/src/main/java/controllers/Converter.java
@@ -5,29 +5,49 @@ import entity.Inproceedings;
 
 /**
  * Converts a Reference Instance to bibTex form.
- * 
  */
 public class Converter {
     /**
-     * 
+     * Creates bibTeX form of Inproceedings instance.
      * @param inpro an Inproceedings instance.
      * @return bibTex form of Inproceedings instance.
      */
     public String toBibTex(Inproceedings inpro){
+        
+        
         String apu = inpro.getAuthor();
         if (apu.length()>3){
             apu = apu.substring(0,3);
         }
         StringBuilder text = new StringBuilder("@inproceedings{");
-        text.append(apu).append(":").append(inpro.getYear()); 
-        text.append(",\n    ").append("author = {").append(inpro.getAuthor());
-        text.append("},\n    ").append("title = {").append(inpro.getTitle());
-        text.append("},\n    ").append("booktitle = {").append(inpro.getBooktitle());
+        text.append(repSpecChars(apu)).append(":").append(inpro.getYear()); 
+        text.append(",\n    ").append("author = {").append(repSpecChars(inpro.getAuthor()));
+        text.append("},\n    ").append("title = {").append(repSpecChars(inpro.getTitle()));
+        text.append("},\n    ").append("booktitle = {").append(repSpecChars(inpro.getBooktitle()));
         text.append("},\n    ").append("year = {").append(inpro.getYear());
         text.append("}\n}");
         
         return text.toString();
     }
-    
+    /**
+     * Search and replace special characters.
+     * @param word is text which should be modified.
+     * @return text with replaced special characters.
+     */
+    private String repSpecChars(String word){
+        StringBuilder str = new StringBuilder();
+        for (char c: word.toCharArray()){
+            switch (c){
+                case 'Ä': str.append("\\\"{A}"); break;
+                case 'Ö': str.append("\\\"{O}"); break;
+                case 'Å': str.append("\\\"AA");  break;
+                case 'ä': str.append("\\\"{a}"); break;
+                case 'ö': str.append("\\\"{o}"); break;
+                case 'å': str.append("\\\"aa");  break;
+                default : str.append(c);       break;
+            }         
+        }
+        return str.toString();
+    }
     
 }
--- Miniprojekti/src/test/java/com/mycompany/miniprojekti/TestConverter.java
+++ Miniprojekti/src/test/java/com/mycompany/miniprojekti/TestConverter.java
@@ -10,11 +10,16 @@ import static org.junit.Assert.*;
  * @author ivantadic
  */
 public class TestConverter {
+        Converter convert; 
+        Inproceedings inpro;
+    @Before
+    public void setUp(){
+        convert = new Converter();
+        inpro = new Inproceedings();
+    }
     
     @Test
-    public void toBibTexTest(){
-        Converter convert = new Converter();
-        Inproceedings inpro = new Inproceedings();
+    public void toBibTexTestNormal(){
         inpro.setAuthor("Roumani, Hamzeh"); 
         inpro.setTitle("Design guidelines for the lab component of objects-first CS1");
         inpro.setBooktitle("SIGCSE '02: Proceedings of the 33rd SIGCSE technical symposium on Computer science education" );
@@ -27,6 +32,24 @@ public class TestConverter {
         
         System.out.println(expValue);
         System.out.println(convert.toBibTex(inpro));
+        assertEquals(expValue, convert.toBibTex(inpro));
+    }
+    
+    
+    @Test
+    public void toBibTexTestSpecCharacters(){
+        inpro.setAuthor("Hassinen, Marko and Mäyrä, Hannu"); 
+        inpro.setTitle("Learning programming by programming: a case study");
+        inpro.setBooktitle("Baltic Sea '06: Proceedings of the 6th Baltic Sea conference on Computing education research: Koli Calling 2006");
+        inpro.setYear(2006);
+        
+        String expValue;
+        expValue = "@inproceedings{Has:2006,\n    author = {Hassinen, Marko and M\\\"{a}yr\\\"{a}, Hannu},\n    " +
+         "title = {Learning programming by programming: a case study},\n    " +
+         "booktitle = {Baltic Sea '06: Proceedings of the 6th Baltic Sea conference on Computing education research: Koli Calling 2006},\n    " +
+         "year = {2006}\n}";
+        //System.out.println(expValue);
+        //System.out.println(convert.toBibTex(inpro));
         assertEquals(expValue, convert.toBibTex(inpro));
     }
     
