53d790a6924c844cabeb49216717804c4058c575
Niko <niko.sikkila@helsinki.fi>
2013-04-11 12:05:13 +0300

    bugikorjaus Inproceedingsiin

--- Miniprojekti/src/main/java/entity/Inproceedings.java
+++ Miniprojekti/src/main/java/entity/Inproceedings.java
@@ -12,7 +12,7 @@ public class Inproceedings extends BaseEntity implements Reference {
 
     private String referenceId;
     private List<Field> fields;
-
+    
     public Inproceedings(List<Field> fields) {
         this.fields = fields;
     }
@@ -35,7 +35,7 @@ public class Inproceedings extends BaseEntity implements Reference {
 
         for (Field field : fields) {
             if (field.getKey().equals(fieldName)) {
-                field.setKey(fieldValue);
+                field.setValue(fieldValue);
                 return;
             }
         }
@@ -43,8 +43,9 @@ public class Inproceedings extends BaseEntity implements Reference {
 
     @Override
     public String getFieldValue(String fieldName) {
-
+        
         for (Field field : fields) {
+            
             if (field.getKey().equals(fieldName)) {
                 return field.getValue();
             }
@@ -52,28 +53,29 @@ public class Inproceedings extends BaseEntity implements Reference {
         return null;
     }
 
+    @Override
     public List<Field> getFields() {
         return fields;
     }
 
     private List<Field> myFields() {
-        List<Field> fields = new ArrayList<Field>();
-        fields.add(new Field("author", true));
-        fields.add(new Field("title", true));
-        fields.add(new Field("booktitle", true));
-        fields.add(new Field("year", true));
+        List<Field> myFields = new ArrayList<Field>();
+        myFields.add(new Field("author", true));
+        myFields.add(new Field("title", true));
+        myFields.add(new Field("booktitle", true));
+        myFields.add(new Field("year", true));
 
-        fields.add(new Field("editor", false));
-        fields.add(new Field("volume/number", false));
-        fields.add(new Field("series", false));
-        fields.add(new Field("pages", false));
-        fields.add(new Field("address", false));
-        fields.add(new Field("month", false));
-        fields.add(new Field("organization", false));
-        fields.add(new Field("publisher", false));
-        fields.add(new Field("note", false));
-        fields.add(new Field("key", false));
+        myFields.add(new Field("editor", false));
+        myFields.add(new Field("volume/number", false));
+        myFields.add(new Field("series", false));
+        myFields.add(new Field("pages", false));
+        myFields.add(new Field("address", false));
+        myFields.add(new Field("month", false));
+        myFields.add(new Field("organization", false));
+        myFields.add(new Field("publisher", false));
+        myFields.add(new Field("note", false));
+        myFields.add(new Field("key", false));
 
-        return fields;
+        return myFields;
     }
 }
